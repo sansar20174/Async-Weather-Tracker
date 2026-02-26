2d3a3e56d33e532ef59e00aac274c20f6229f869
Sansar Kumar <kumarsansar999@gmail.com>
2026-02-26 15:32:47 +0530

    Refactor weather search functionality and enhance search history display

--- AQI.js
+++ AQI.js
@@ -1,37 +1,65 @@
 const API_KEY = "ca2fb287aff257a252c222ee96b556a6";
 
 const city = document.querySelector("#city");
-const search = document.querySelector("#search");
-visitCites = [];
+const search = document.querySelector("#weatherForm");
+const history=document.querySelector(".SearchHistory");
+const Container = document.querySelector(".info");
+let visitedCities =[];
 
-search.addEventListener("click", async (e) => {
-  e.preventDefault();
+search.addEventListener("submit", async (e) => {
+    e.preventDefault();
 
-  const data = city.value;
-  if(data){
-    try {
-         const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${data}&appid=${API_KEY}`);
-         const weatherData = await response.json();
+    const data = city.value;
+    searchWeather(data)
+
+})
+
+async function searchWeather(city){
+    if(city){
+        try{
+            const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`);
+            const weatherData = await response.json();
             console.log(weatherData);
-        
-        if(weatherData.cod === 200){
-            container.innerHTML = `<h3>Weather Info</h3>
-            <p>City: ${weatherData.name}</p>
-            <p>Temp: ${(weatherData.main.temp - 273).toFixed(1)} °C</p>
-            <p>Weather: ${weatherData.weather[0].main}</p>
-            <p>Humidity: ${weatherData.main.humidity}</p>
-            <p>Wind: ${weatherData.wind.speed} miles/hr</p>`;
 
-            visitCites.push(weatherData.name);
-            localStorage.setItem("visitedCities", JSON.stringify(visitCites));
-            
-        } else {
-            container.innerHTML = `<h3>Weather Info</h3>
-            <p>${weatherData.message}</p>`
-        } 
-    } catch (error) {
-        console.error("Error fetching weather data:", error);
-        container.innerHTML = `<p>Error fetching weather data. Please try again later.</p>`;
+            if (weatherData.cod === 200) {
+                Container.innerHTML = `
+                <p>City: ${weatherData.name}</p>
+                <p>Temp: ${(weatherData.main.temp - 273.15).toFixed(1)} °C</p>
+                <p>Weather: ${weatherData.weather[0].main}</p>
+                <p>Humidity: ${weatherData.main.humidity}</p>
+                <p>Wind: ${weatherData.wind.speed} miles/hr</p>`;
+                if(!visitedCities.includes(city)){
+                    visitedCities.push(city);
+                    localStorage.setItem("visitedCities", JSON.stringify(visitedCities));
+                }
+                showHistory();
+
+            } else {
+                Container.innerHTML=`<h3>Weather Info Not Available</h3>
+                <p>${weatherData.message}</p>`
+            }
+        }catch(e){
+                console.log(e)
+            }
+        }
     }
-  }
-});
+
+  
+
+function showHistory(){
+    history.innerHTML="";
+    const cities=JSON.parse(localStorage.getItem("visitedCities"))
+    if(cities){
+        cities.forEach((city)=>{
+            const li=document.createElement("button");
+            li.textContent=city;
+            li.addEventListener("click",()=>{
+                searchWeather(city);
+            })
+            history.appendChild(li);
+        })
+    }
+    
+}
+
+showHistory()
